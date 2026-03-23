4fd1f83d69c90fe9f8366a7db53f8f9436cfbaf1
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-03-23 10:55:09 +0100

    feat : job05

---
+++ jour01/job05/script.js
@@ -0,0 +1,8 @@
+function afficherJoursSemaines() {
+    const tabJours = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"];
+    for(let i = 0 ; i < tabJours.length ; i++) {
+        console.log(tabJours[i]);
+    }
+}
+
+afficherJoursSemaines();
