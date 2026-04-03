400d08d281ef73854c697ca9de692339c0abc95c
lorenzo-brissardnavarro <lorenzo.brissardnavarro@laplateforme.io>
2026-04-03 12:37:48 +0200

    feat : gestion des droits

--- bigjob/js/backoffice.js
+++ bigjob/js/backoffice.js
@@ -8,6 +8,7 @@ if(!user || user.role !== "moderator"){
 }
 
 const tableBody = document.getElementById("table-body");
+const tableBodyRights = document.getElementById("table-body-rights");
 
 function updateBackofficeTable() {
     const users = JSON.parse(localStorage.getItem("users"));
@@ -32,8 +33,8 @@ function updateBackofficeTable() {
             let actionHtml = "";
             if (statut === "en attente") {
                 actionHtml = `
-                    <button class="accept bg-green-500 hover:bg-green-600 text-white px-3 py-1 rounded-lg mr-2" data-email="${user.email}" data-date="${date}">Accepter</button>
-                    <button class="refuse bg-red-500 hover:bg-red-600 text-white px-3 py-1 rounded-lg" data-email="${user.email}" data-date="${date}">Refuser</button>
+                    <button class="accept bg-green-500 hover:bg-green-600 cursor-pointer text-white px-3 py-1 rounded-lg mr-2" data-email="${user.email}" data-date="${date}">Accepter</button>
+                    <button class="refuse bg-red-500 hover:bg-red-600 cursor-pointer text-white px-3 py-1 rounded-lg" data-email="${user.email}" data-date="${date}">Refuser</button>
                 `;
             }
             row.innerHTML = `
@@ -63,15 +64,66 @@ function attachButtonsEvents() {
 }
 
 function handleDecision(email, date, decision) {
-    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const users = JSON.parse(localStorage.getItem("users"));
     const user = users.find(u => u.email === email);
 
-    if (!user || !user.calendar[date]) return;
-
+    if (!user || !user.calendar[date]){
+        return;
+    }
     user.calendar[date] = decision;
     localStorage.setItem("users", JSON.stringify(users));
 
     updateBackofficeTable();
 }
 
+function updateRightsTable() {
+    const users = JSON.parse(localStorage.getItem("users"));
+    tableBodyRights.innerHTML = "";
+
+    users.forEach(user => {
+        const row = document.createElement("tr");
+        let currentRoleColor = "";
+        switch(user.role) {
+            case "moderator":
+                currentRoleColor = "bg-yellow-200 text-yellow-800";
+                break;
+            case "user":
+                currentRoleColor = "bg-gray-200 text-gray-800";
+                break;
+        }
+        row.innerHTML = `
+            <td class="p-4 border-b border-slate-300">${user.email}</td>
+            <td class="p-4 border-b border-slate-300">${user.role}</td>
+            <td class="p-4 border-b border-slate-300"><button class="role bg-green-500 hover:bg-green-600 cursor-pointer text-white px-3 py-1 rounded-lg mr-2" data-email="${user.email}">Changer le rôle</button></td>
+        `;
+
+        tableBodyRights.appendChild(row);
+    });
+    RoleButton();
+}
+
+function RoleButton() {
+    const roleButtons = document.querySelectorAll(".role");
+
+    roleButtons.forEach(btn => {
+        btn.addEventListener("click", () => ChangeRole(btn.dataset.email));
+    });
+}
+
+function ChangeRole(email) {
+    const users = JSON.parse(localStorage.getItem("users"));
+    const user = users.find(u => u.email === email);
+
+    if (!user || !user.role){
+        return;
+    }
+
+    const decision = user.role === "user" ? "moderator" : "user";
+    user.role = decision;
+    localStorage.setItem("users", JSON.stringify(users));
+
+    updateRightsTable();
+}
+
 window.addEventListener("load", updateBackofficeTable);
+window.addEventListener("load", updateRightsTable);
